14bb708a55161cc1d7ea33d5ad2bb5b4324172c2
iimad <alisawiali70@gmail.com>
2022-11-12 19:04:09 +0100

    Attribute mit setAttribute && removeAttribute && attributes

---
+++ main.js
@@ -0,0 +1,35 @@
+let imad =document.getElementById('pecker');
+pecker.className='perp'
+pecker.setAttribute('alt','Imad') /* wechselt die namen */
+
+
+
+
+
+  console.log(pecker.attributes[2])
+// console.log(pecker.hasAttribute('alt'))/* hasAttribute das ist die Bestätigung, ob wir eine Attribut haben oder nicht  zb.('alt') 
+
+
+// wir können mit einem wort sehen , ob alle Attribut da sind oder nicht und es war ( Attribbutes ) und wir können das Erste Attribut haben mit [0] und soweite
+// */
+
+/*mit setAttribute ('alt', 'imad') ändern wir die Nmaen
+ */
+console.log('------------setAttribute---------------')
+/*
+pecker.setAttribute('src','Alisawi')
+console.log(pecker.titel)
+console.log(pecker.attributes[1])*/
+
+('------------removeAttribute---------------')
+
+/* wenn wir ein Element löschen wollten dann schreiben wir (removeAttribute) */
+
+console.log
+pecker.removeAttribute('alt')
+console.log(pecker)
+
+pecker.removeAttribute('class')
+
+// pecker.removeAttribute('src')
+
